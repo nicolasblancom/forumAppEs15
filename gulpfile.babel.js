d84ea6b2226b3039e7f83766f2b4418169a59ad3
nicolasblancom <nicolasblancom@users.noreply.github.com>
2016-06-28 20:27:23 +0200

    task watch para gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,8 @@ import gulp from "gulp";
 import browserify from "browserify";
 import source from "vinyl-source-stream";
 
-gulp.task("default", () => {
+// transpiling task
+gulp.task("transpile", () => {
 
   return browserify("src/app.js")
     .transform("babelify")
@@ -12,3 +13,10 @@ gulp.task("default", () => {
 
 });
 
+// add watch task, run transpile before start watching
+gulp.task("watch", ["transpile"], () =>{
+	gulp.watch("src/**/*.js", ["transpile"]);
+});
+
+// default task
+gulp.task("default", ["transpile"]);
